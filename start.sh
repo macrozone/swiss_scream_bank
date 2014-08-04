24669cbb18db063e87dde2efdacea15411082981
Marco <macrozone@gmail.com>
2014-08-05 00:17:25 +0200

    port

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export MONGO_URL=mongodb://localhost/swiss_scream_bank
 export ROOT_URL=http://swiss-scream-bank.macrozone.ch
-export PORT=8120
+export PORT=8130
 export METEOR_SETTINGS=$(cat settings.prod.json)
 forever start $DIR/bundle/main.js
